19d6b09b8ac01e35f526ea0142107be285fd261f
devin5885 <devin5885@gmail.com>
2016-05-10 17:35:36 -0700

    Complete Chapter 7

--- ASP.NET for VS 2015/Working/RentMyWrox/RentMyWrox/Admin/ManageItem.aspx.cs
+++ ASP.NET for VS 2015/Working/RentMyWrox/RentMyWrox/Admin/ManageItem.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace RentMyWrox.Admin
 {
-    public partial class ManageItem : System.Web.UI.Page
+    public partial class ManageItem : WebFormsBaseClass
     {
         protected void Page_Load(object sender, EventArgs e)
         {
